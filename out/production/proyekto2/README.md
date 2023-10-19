e7dc69731859e4035cd39e0272dd3d3089c0dc35
Maiztegui <webnmaiztegi@imaltuna.com>
2023-10-19 12:32:05 +0200

    verioa2

---
+++ out/production/proyekto2/README.md
@@ -0,0 +1,3 @@
+# hello-world.
+Lehenego repositorioa
+Soy pelirroja, me encantan los animales, adoro a los gatos y quiro uno de cada color. 
---
+++ proiektua.java
@@ -0,0 +1,5 @@
+public class proiektua {
+    public static void main(String[] args) {
+        System.out.printf("No entiendo una puta mierda !");
+    }
+}
